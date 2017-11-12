ae298bbc20f0e7778729b715f742d2cc171c077b
Dave Vosk <dvoskanyan2@gmail.com>
2017-11-12 22:00:37 +0400

    redux implementation

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View, } from 'react-native';
 import { TabNavigator, StackNavigator } from 'react-navigation';
+import { Provider } from 'react-redux';
 
+import store from './store';
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
 import DeckScreen from './screens/DeckScreen';
@@ -13,7 +15,7 @@ class App extends React.Component {
   render() {
 
     const MainNavigator = TabNavigator({
-      welcome: { screen: WelcomeScreen, },
+      // welcome: { screen: WelcomeScreen, },
       auth: { screen: AuthScreen },
       main: {
         screen: TabNavigator({
@@ -30,9 +32,11 @@ class App extends React.Component {
     });
 
     return (
-      <View style={styles.container}>
-        <MainNavigator />
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <MainNavigator />
+        </View>
+      </Provider>
     );
   }
 }
