ed6d6312cc9649e01fb95adac499a3a00aa622f2
David <dvoskanyan2@gmail.com>
2017-11-25 23:26:56 +0400

    redux-persist implementation

--- reducers/index.js
+++ reducers/index.js
@@ -1,9 +1,17 @@
-import { combineReducers } from 'redux';
+// import { combineReducers } from 'redux';
 import auth from './auth_reducer';
 import jobs from './jobs_reducer';
 import likedJobs from './like_reducer';
 
-export default combineReducers({
+import { persistCombineReducers } from 'redux-persist'
+import storage from 'redux-persist/es/storage'
+
+const config = {
+  key: 'primary',
+  storage
+};
+
+export default persistCombineReducers(config, {
   auth,
   jobs,
   likedJobs
--- store/index.js
+++ store/index.js
@@ -1,13 +1,23 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { persistStore } from 'redux-persist'
+
 import reducers from '../reducers';
 
 const store = createStore(
   reducers,
   {},
   compose(
-    applyMiddleware(thunk)
+    applyMiddleware(thunk),
   )
-)
+);
+
+persistStore(
+  store,
+  null,
+  () => {
+    store.getState()
+  }
+);
 
 export default store;
