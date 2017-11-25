b9bfffcf595100a62ab9228081592e7db2197c0a
David <dvoskanyan2@gmail.com>
2017-11-26 00:57:59 +0400

    correct auth_reducer

--- reducers/auth_reducer.js
+++ reducers/auth_reducer.js
@@ -1,11 +1,11 @@
 import * as aT from '../actions/types';
 
-export default (state = { token: 'gggjj' }, action) => {
+export default (state = { token: null }, action) => {
   switch (action.type) {
     case aT.FACEBOOK_LOGIN_SUCCESS:
       return { token: action.payload };
     case aT.FACEBOOK_LOGIN_FAIL:
-      return { toke: null };
+      return { token: null };
 
   }
   return state;
